6cd548a4a4e968e6ef64ca630fa3a07e42dadfe5
KohTongLiang <KohTongLiang@users.noreply.github.com>
2021-06-24 16:16:56 +0800

    Delete FrontEnd directory

--- FrontEnd/frats_system_ase/ciscripts/noweb.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-echo "Hiding any web only files from being compiled"
-
-# Do the replacement here
-echo "No Files to replace"
-# End replacement
-
-echo "Web files hidden"
--- FrontEnd/frats_system_ase/ciscripts/web.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-echo "Showing any web only files to be compiled"
-
-# Do the replacement here
-echo "No Files to replace"
-# End replacement
-
-echo "Web files shown"
